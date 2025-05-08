c4c17a3bdad94214a8306678a12664e4dff7025a
David Rojo <davidrojo1997@gmail.com>
2025-05-09 01:52:14 +0200

    feat: add scaffolding

---
+++ src/content/config.ts
@@ -0,0 +1,8 @@
+import { defineCollection } from "astro:content";
+import { z } from "astro:content";
+
+// Add movies collection here
+
+export const collections = {
+  // export movies collection
+};
