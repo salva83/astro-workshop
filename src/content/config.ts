56241ef8da105e5f8d42f9642f46a48abd0177fa
Salvador Fernandez <salvador.fernandez@gig.com>
2025-05-09 18:53:05 +0200

    in progress

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,21 @@
 import { defineCollection } from "astro:content";
 import { z } from "astro:content";
 
+
 // Add movies collection here
+const movies = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    director: z.string(),
+    date: z.string(),
+    image: z.string(),
+    slug: z.string(),
+  }),
+});
+
 
 export const collections = {
   // export movies collection
+  movies
 };
+
