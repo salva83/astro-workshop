b78a12c13240886b64e4db45ff17f3310d5f7a76
Salvador Fernandez <salvador.fernandez@gig.com>
2025-05-09 19:44:12 +0200

    set action and handler for rating and hydate

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,5 +2,14 @@ import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
 export const server = {
-  // add rate action
+    // add rate action
+    rate: defineAction({
+        input: z.object({
+            id: z.string(),
+            rate: z.enum(["bad", "good", "none"]),
+        }),
+        handler: (params) => {
+            console.log(params);
+        },
+    }),
 };
