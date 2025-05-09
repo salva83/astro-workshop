11785252f28ae95691ab2495d796427cbe679675
Salvador Fernandez <salvador.fernandez@gig.com>
2025-05-09 19:33:50 +0200

    setup of template for credentials , incorrect field description

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import {defineConfig, envField} from "astro/config";
 
 // @ts-check
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-// pending to define env under defineConfig , config
+// pending to define env under defineConfig , config -> to be fixed second part
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
+  env: {
+    schema: {
+      MOVIES_URL: envField.string({
+        context: "server",
+        description: "server",
+      }),
+      }
+    }
+  }
 
 });
