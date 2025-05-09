a0f143a8f3a2ddb73d0d384e78a1401efb1c5665
Salvador Fernandez <salvador.fernandez@gig.com>
2025-05-09 19:28:16 +0200

    random movie card from external resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+// @ts-check
+import { defineConfig } from "astro/config";
+
 import tailwindcss from "@tailwindcss/vite";
 
+// pending to define env under defineConfig , config
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
+
 });
